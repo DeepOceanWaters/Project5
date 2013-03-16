0d195b20c78374ac80283570ff1829eb35b63842
Colin Bradford <cjthehippo@gmail.com>
2013-03-16 04:35:57 -0700

    Added the parse_msg function.
    parse_msg(msg):
    	Description:
    		parse_msg() takes a string and parses the string into an array of values separated by tabs
    	params:
    		msg: the string to be parsed

--- files/manage.py
+++ files/manage.py
@@ -4,6 +4,14 @@ import re
 def main():
 	
 
+## parse_msg:
+## 	takes a string, and parses it into an array of tab seperated values
+## params:
+## 	msg: the string to be parsed
+##
+def parse_msg(msg):
+	tab_regex = re.compile(".*?\t+")
+	return tab_regex.findall(msg)
 
 
 if __name__ == "__main__":
