8a17f969b55d656ecf434137b87af390a394e91e
Colin Bradford <cjthehippo@gmail.com>
2013-03-16 04:45:00 -0700

    Added the kill_compute function.
    kill_compute(sock):
    	Description:
    		Takes a socket connected to a compute process and sends it a kill message.
    		The compute process(es) should then shutdown.
    	params:
    		sock: a socket connected to a compute process.

--- files/manage.py
+++ files/manage.py
@@ -4,6 +4,15 @@ import re
 def main():
 	
 
+## kill_compute:
+## 	takes a socket connected to a compute process, and sends a kill message
+##	to the socket. The compute process should then terminate.
+## params:
+## 	sock: socket connected to the compute process
+def kill_compute(sock):
+	kill_msg = "shutdown"
+	sock.send(kill_msg)
+
 ## parse_msg:
 ## 	takes a string, and parses it into an array of tab seperated values
 ## params:
