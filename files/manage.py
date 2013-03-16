b63b275c4bc1f8e363a5056972926ef9cdea2975
Colin Bradford <cjthehippo@gmail.com>
2013-03-16 04:51:06 -0700

    Added create_sock_stream function
    create_sock_stream():
    	Description:
    		Creates a new stream socket.

--- files/manage.py
+++ files/manage.py
@@ -4,11 +4,18 @@ import re
 def main():
 	
 
+## create_sock:
+## 	returns a newly created socket. Socket uses AF_INET and SOCK_STREAM
+##
+def create_sock_stream():
+	return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 ## kill_compute:
 ## 	takes a socket connected to a compute process, and sends a kill message
 ##	to the socket. The compute process should then terminate.
 ## params:
 ## 	sock: socket connected to the compute process
+##
 def kill_compute(sock):
 	kill_msg = "shutdown"
 	sock.send(kill_msg)
